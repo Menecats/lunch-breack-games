0335836e9d1700d677d761b9fce5be239b0956b2
Menecats <d.menegatti@recrosoftware.com>
2018-05-14 14:05:22 +0200

    - Player throw - WIP

--- Week 01 - OiledDwarf/Assets/Scripts/PlayerThrow.cs
+++ Week 01 - OiledDwarf/Assets/Scripts/PlayerThrow.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerThrow : MonoBehaviour {
+	public GameObject projectlePrefab;
+	public Transform firePoint;
+
+	public float modifier = 1f;
+
 	Camera camera;
 
-	void Start () {
+	void Start() {
 		camera = GetComponentInChildren<Camera>();
 	}
 	
-	void Update () {
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+        	Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+			if (Physics.Raycast(ray, out hit)) {
+				Transform objectHit = hit.transform;
+
+				ThrowItem(objectHit.position);
+			}
+		}
+	}
+
+	void ThrowItem(Vector3 target) {
+		GameObject projectle = Instantiate(projectlePrefab, firePoint.position, firePoint.rotation);
+		Rigidbody rb = projectle.GetComponent<Rigidbody>();
+
+		Vector3 dir = target - projectle.transform.position;
+
+		rb.AddForce(target * modifier, ForceMode.VelocityChange);
 	}
 }
