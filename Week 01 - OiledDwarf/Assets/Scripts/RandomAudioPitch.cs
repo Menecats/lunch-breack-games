af70766d50a031013d4b8eed3184aff495dd657d
Menecats <d.menegatti@recrosoftware.com>
2018-05-21 14:08:28 +0200

    Portion audio random pitch, lightning

---
+++ Week 01 - OiledDwarf/Assets/Scripts/RandomAudioPitch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAudioPitch : MonoBehaviour {
+	public float minPitch = .8f;
+	public float maxPitch = 1.2f;
+
+	void Start () {
+		GetComponent<AudioSource>().pitch = Random.Range(minPitch, maxPitch);
+	}
+}
