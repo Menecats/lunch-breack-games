ba78aad08d792cc8aa21139377b39cabdbefe691
Menecats <d.menegatti@recrosoftware.com>
2018-05-29 14:12:48 +0200

    Added throw item cooldown

--- Week 01 - OiledDwarf/Assets/Scripts/PlayerThrow.cs
+++ Week 01 - OiledDwarf/Assets/Scripts/PlayerThrow.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerThrow : MonoBehaviour
 {
     public GameObject projectlePrefab;
     public Transform firePoint;
 
+    public float throwDelay = .5f;
+
+    public Image coutdownImage;
+
+    public float throwCooldown = 0f;
     public float modifier = 3f;
     public float adjustAngle = 30f;
 
@@ -20,6 +24,19 @@ public class PlayerThrow : MonoBehaviour
 
     void Update()
     {
+        if (throwCooldown > 0)
+        {
+            throwCooldown -= Time.deltaTime;
+            if (throwCooldown <= 0)
+            {
+                throwCooldown = 0;
+            }
+
+            coutdownImage.fillAmount = throwCooldown / throwDelay;
+
+            return;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -34,6 +51,8 @@ public class PlayerThrow : MonoBehaviour
 
     void ThrowItem(Vector3 target)
     {
+        throwCooldown = throwDelay;
+
         GameObject projectle = Instantiate(projectlePrefab, firePoint.position, firePoint.rotation);
         Rigidbody rb = projectle.GetComponent<Rigidbody>();
 
